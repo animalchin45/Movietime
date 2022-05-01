e203da0122d38b6911c63056f2581d9ce0542844
Tim O'Brien <timobrien.inc@gmail.com>
2022-05-01 16:09:16 -0400

    initial commit, basic non user functions complete,  alpha v0.5

---
+++ webpack.config.client.js
@@ -0,0 +1,77 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const webpack = require('webpack')
+const dotenv = require('dotenv-webpack')
+const CURRENT_WORKING_DIR = process.cwd()
+const config={
+    name: 'browser',
+    mode: 'development',
+    devtool: 'eval-source-map',
+    entry: [
+        'webpack-hot-middleware/client?reload=true',
+        path.join(CURRENT_WORKING_DIR,'client/index.js')
+    ],
+    output:{
+        path: path.join(CURRENT_WORKING_DIR, '/dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist/'
+    },
+    module:{
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {       
+                test: /\.(png|jpg|gif)$/,
+                use: [{
+                    loader: 'file-loader',
+                    // options: {
+                    //     outputPath: 'img',
+                    //     publicPath: 'img'
+                    // }
+                }]
+            },
+            {
+                test: /\.svg$/,
+                use: ['@svgr/webpack'],
+            },
+            {  
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns: [
+                '**/*'
+            ]
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin(),
+        new dotenv()
+    ],
+    resolve:{
+    alias:{
+        'react-dom': '@hot-loader/react-dom'
+        }
+    }
+}
+module.exports = config
