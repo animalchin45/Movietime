a74194f7a7fe58f95c6ea92fd721fd1cb4fcbdd8
Tim O'Brien <timobrien.inc@gmail.com>
2022-06-09 15:28:36 -0400

    move toastify css into project and import

--- client/components/App.js
+++ client/components/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
 import { hot } from 'react-hot-loader/root'
 
 import Layout from './Layout'
