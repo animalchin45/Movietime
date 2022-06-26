ca924ca723a3c6bcfc455ce4409d64094c8722fb
Tim O'Brien <timobrien.inc@gmail.com>
2022-06-26 11:38:48 -0400

    adjust mobile menu size, fixed uppercase email issue

--- client/components/App.js
+++ client/components/App.js
@@ -31,7 +31,7 @@ const App = () => {
             </BrowserRouter>
             <ToastContainer 
                 pauseOnFocusLoss={false} 
-                autoClose={3000}    
+                autoClose={1500}    
             />
         </>
     )
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -61,9 +61,10 @@ const registerUser = asyncHandler(async (req, res) => {
 // @access  Public
 const loginUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body
+    let lowerCaseEmail = email.toLowerCase()
 
     // Check for email
-    const user = await User.findOne({email})
+    const user = await User.findOne({lowerCaseEmail})
 
     if (user && (await bcrypt.compare(password, user.password))) {
         res.json({
